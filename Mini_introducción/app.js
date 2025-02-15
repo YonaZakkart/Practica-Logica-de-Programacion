62fca2fdec14393cccbcef638ce58f4974869aad
YonaZakkart <musicyonathan.com>
2025-02-15 15:31:29 -0600

    Agregar Practica de laboratorio

---
+++ Mini_introducción/app.js
@@ -0,0 +1,19 @@
+
+const Users = [
+    {
+        fullname: "Yonathan Sanchez",
+        email: 'yonathansanchez@gmail.com',
+        password: "12345",
+        age: 20
+    },
+    {
+        fullname: "Edwin Lopez",
+        email: 'edwinjoselopez@gmail.com',
+        password: "1234",
+        age: 18
+    }
+]
+
+for(let item of Users) {
+    console.log(item.fullname);
+} 
